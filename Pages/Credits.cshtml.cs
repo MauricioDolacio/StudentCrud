3abc1ca05df29ef7689f08da6d5b78826391ee23
mauriciodolacio <mauriciodolacio@outlook.com>
2024-03-04 03:25:08 -0300

    Publish project in Github

---
+++ Pages/Credits.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentCrud.Pages;
+
+public class CreditsModel : PageModel
+{
+    private readonly ILogger<CreditsModel> _logger;
+
+    public CreditsModel(ILogger<CreditsModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
